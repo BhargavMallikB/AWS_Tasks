5f218f48dbbfe722b0a10b85cf7757978438dcd7
BhargavMallikB <bhargavmallikb@gmail.com>
2025-04-22 16:24:50 +0530

    Code Updated that can be Testable

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { BASE_URL } from "./consts/URL"; // BASE_URL used to fetch data
 import { User } from "./types/Users"; // User is a type of data that we retreive
 import axios, { AxiosResponse } from "axios"; // axios for invoking BASE_URL with different methods GET, PORT ..
 
-export async function getUsers(): Promise<void> { // returns Promise of User array
+export async function getUsers(): Promise<User[] | undefined> { // returns Promise of User array
      
     try{ // Handling Errors using try & catch blocks
         const response: AxiosResponse<User[]> = await axios.get<User[]>(BASE_URL); // Invoking BASE_URL to retreive and store the response
-        console.table(response.data); // prints the data that gets from the response in a tabular format
+        return response.data; // prints the data that gets from the response in a tabular format
     } catch (err) { 
         console.error(`Error occured while Fetching Users ${err}`); // any error occurs prints in the console
     }
 }
 
-getUsers(); // Invokes above function to retreive the data
+console.table(getUsers()); // Invokes above function to retreive the data
